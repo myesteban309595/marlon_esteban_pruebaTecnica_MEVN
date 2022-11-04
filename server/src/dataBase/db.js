e97ce1357c6ff27b890b20903684df64e1818740
myesteban309595 <maryoe_95@hotmail.com>
2022-11-04 18:40:29 -0500

    creacion modelo user

--- server/src/dataBase/db.js
+++ server/src/dataBase/db.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose')
-const config = require('../config/config')
+const config = require('../config/config');
+const userModel = require('../models/user.model');
 
 const DB_CREDENTIALS_PASSWORD = config.module.DB_CREDENTIALS_PASSWORD;
 const DB_CREDENTIALS_USER = config.module.DB_CREDENTIALS_USER;
 
-const DB_URL_CONNECTION = `mongodb+srv://${DB_CREDENTIALS_USER}:${DB_CREDENTIALS_PASSWORD}@cluster0.tx4uwtg.mongodb.net/?retryWrites=true&w=majority` ;
+const DB_URL_CONNECTION = `mongodb+srv://${DB_CREDENTIALS_USER}:${DB_CREDENTIALS_PASSWORD}@cluster0.groubjk.mongodb.net/?retryWrites=true&w=majority` ;
 
 console.log(DB_URL_CONNECTION);
 (async()=>{
@@ -18,5 +19,15 @@ console.log(DB_URL_CONNECTION);
     .catch((error) => {
         console.error(error)
     });
-    
+
+    const existingUser = await userModel.find();
+
+    if(existingUser.length === 0)
+    {
+        const user1 = new userModel({
+           email: "maryoe_95@hotmail.com",
+           password: "admin123"
+        }) 
+        user1.save()
+    }
 })();
---
+++ server/src/models/user.model.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    email: {
+        type: String,
+        require: true
+    },
+    password: {
+        type: String,
+        require: true
+    }
+})
+
+module.exports = mongoose.model('user', userSchema)
